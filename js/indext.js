5f071fa6d62a0b77aae00c349dafccf66b94e27d
kasra <kasramajdy81@gmail.com>
2025-01-28 12:33:35 +0330

    Create a function that checks if a number is prime.

---
+++ js/indext.js
@@ -0,0 +1,11 @@
+function inputNumber(num){
+    if (num <= 1){
+        return console.log("عدد اول نیست")
+    } else if (num % 2 === 0){
+        return console.log("عدد اول نیست")
+    } else{
+        return console.log("عدد اول است")
+    }
+}
+
+inputNumber(3)
